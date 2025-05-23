b019fe229c9e6234b546516fe6a1f4ec64d34aa7
Andrei Simedre <andrei.simedre@gmail.com>
2025-05-23 10:37:27 +0300

    Init project

---
+++ VideoPlay/VideoPlay/VideoPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayApp.swift
+//  VideoPlay
+//
+//  Created by Andrei Simedre on 23.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlay/VideoPlayTests/VideoPlayTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayTests.swift
+//  VideoPlayTests
+//
+//  Created by Andrei Simedre on 23.05.2025.
+//
+
+import Testing
+@testable import VideoPlay
+
+struct VideoPlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
